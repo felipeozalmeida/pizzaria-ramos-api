ff6c2ff63fa362bb59c70d65175967bf0ad58ae4
Felipe Almeida <felipeortalmeida@gmail.com>
2019-10-12 23:02:29 -0300

    Made first migration w/ basic fields

--- pizzaria_ramos_api/pizzaria_ramos_api/models.py
+++ pizzaria_ramos_api/pizzaria_ramos_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=60)
+    ingredients = models.TextField()
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    # TODO: add field for uploaded image
+
