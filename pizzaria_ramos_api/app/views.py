d16fbfae0e750f4ccb40e481a0b131714ee6cfa0
Felipe Almeida <felipeortalmeida@gmail.com>
2019-10-26 19:56:53 -0300

    Added pizza search by name

--- pizzaria_ramos_api/app/views.py
+++ pizzaria_ramos_api/app/views.py
@@ -1,8 +1,10 @@
 from pizzaria_ramos_api.app.models import Pizza
 from pizzaria_ramos_api.app.serializers import PizzaSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 # Create your views here.
 class PizzaViewSet(viewsets.ModelViewSet):
     queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
