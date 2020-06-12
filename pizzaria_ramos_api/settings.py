2257292e494f21460b804bda5e7bd37c106ba445
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-12 01:35:15 -0300

    Changed DB to use SQLite

--- pizzaria_ramos_api/settings.py
+++ pizzaria_ramos_api/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'pizzaria_ramos_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pizzaria_ramos_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
